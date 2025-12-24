da2eef518807bba2521173bbe4203706d457450d
afuentes <alex@glyfo.com>
2025-12-24 09:49:50 -0700

    improve it

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -61,12 +61,12 @@ function App() {
                 const Icon = feature.icon
                 return (
                   <div key={idx} className="flex gap-4 group">
-                    <div className="flex-shrink-0">
+                    <div className="shrink-0">
                       <div className="flex items-center justify-center h-10 w-10 rounded-md bg-slate-100 text-slate-900 group-hover:bg-blue-100 group-hover:text-blue-900 transition-colors">
                         <Icon size={20} />
                       </div>
                     </div>
-                    <div className="flex-grow">
+                    <div className="grow">
                       <h3 className="text-sm font-semibold text-slate-900">{feature.title}</h3>
                       <p className="text-sm text-slate-600 mt-0.5">{feature.description}</p>
                     </div>
